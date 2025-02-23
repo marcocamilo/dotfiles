ac28242646646cd98142be8cac6d0620142070ee
Marco Camilo <mac.pietri10@gmail.com>
2025-02-23 19:29:18 +0100

    dwm and zsh cleanup

--- zsh/.config/zsh/environments.zsh
+++
@@ -1,8 +0,0 @@
-#  ────────────────────────────────────────────────────────────────────
-#   PYTHON                                                             
-#  ────────────────────────────────────────────────────────────────────
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
