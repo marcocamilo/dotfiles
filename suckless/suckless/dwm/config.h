7da9331e5558528eed95c6af15f15ef990c3fc5a
Marco Camilo <mac.pietri10@gmail.com>
2025-02-26 22:27:53 +0100

    dwm browser change

--- suckless/suckless/dwm/config.h
+++ suckless/suckless/dwm/config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#define BROWSER "opera"
+#define BROWSER "firefox"
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
