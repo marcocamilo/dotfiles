ee26913e703f729395840557f8b9064651d585f8
Marco Camilo <mac.pietri10@gmail.com>
2025-03-05 12:26:24 +0100

    surround event removed, gcp global variable set, lazygit config

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -6,6 +6,11 @@ export PATH="$PATH:$HOME/.local/bin"
 export PATH="/usr/local/nvim/bin:$PATH"
 # export PATH="/usr/local/"
 
+# ──────────────────────────────────────────────────────────────────────
+#   GCP                                                                
+# ──────────────────────────────────────────────────────────────────────
+export GOOGLE_APPLICATION_CREDENTIALS="/home/Camilo-Pietri.M/service_accounts/camilo-pietri-m.json"
+
 #  ────────────────────────────────────────────────────────────────────
 #   EDITOR                                                             
 #  ────────────────────────────────────────────────────────────────────
