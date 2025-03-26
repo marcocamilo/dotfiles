3b0f971112ebd9e111df1d4c1da2885359823571
Marco Camilo <mac.pietri10@gmail.com>
2025-03-26 21:46:44 +0100

    blink replacement for cmp

--- nvim/.config/nvim/lua/marco/plugins/notifications.lua
+++ nvim/.config/nvim/lua/marco/plugins/notifications.lua
@@ -10,6 +10,7 @@ return {
 		},
 		opts = {
 			lsp = {
+        signature = { auto_open = false },
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
