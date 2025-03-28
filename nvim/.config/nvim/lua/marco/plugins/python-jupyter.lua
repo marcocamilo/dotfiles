cec52cf61c9cd32bb451189162fc82aa8be0ca30
Marco Camilo <mac.pietri10@gmail.com>
2025-03-28 11:10:09 +0100

    blink replacement for cmp

--- nvim/.config/nvim/lua/marco/plugins/python-jupyter.lua
+++ nvim/.config/nvim/lua/marco/plugins/python-jupyter.lua
@@ -37,19 +37,13 @@ return {
 		lazy = false,
 		opts = {
 			style = "hydrogen",
-			-- custom_language_formatting = {
-			-- 	python = {
-			-- 		extension = "md",
-			-- 		style = "markdown",
-			-- 		force_ft = "markdown", -- you can set whatever filetype you want here
-			-- 	},
 			custom_language_formatting = {
 				python = {
 					extension = "qmd",
 					style = "quarto",
-					force_ft = "quarto", -- you can set whatever filetype you want here
+					force_ft = "quarto",
 				},
-			}, -- },
+			},
 		},
 	},
 }
