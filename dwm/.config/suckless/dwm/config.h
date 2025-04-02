f2736cdcd1a231ecb8c83461216bc1749d037b53
Marco Camilo <mac.pietri10@gmail.com>
2025-04-02 18:59:06 +0200

    j4-dmenu-desktop

--- dwm/.config/suckless/dwm/config.h
+++ dwm/.config/suckless/dwm/config.h
@@ -95,8 +95,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+  "j4-dmenu-desktop", NULL
+    // "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    // "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL
+};
 static const char *termcmd[] = {"alacritty", NULL};
 static const char *upvol[]      = { "/home/marcocamilo/.local/bin/volume", "up", NULL };
 static const char *downvol[]    = { "/home/marcocamilo/.local/bin/volume", "down", NULL };
