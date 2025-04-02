f0763b821b8f204d4d7f36ab77047fab83c72470
Marco Camilo <mac.pietri10@gmail.com>
2025-04-02 19:03:10 +0200

    mojo export path

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,6 +4,7 @@
 #
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$HOME/.modular/bin"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
