eb5569961fddb7182a8f9dc1969a422534fcb20f
Marco Camilo <mac.pietri10@gmail.com>
2025-04-02 21:17:41 +0200

    keyboard switcher from dwm branch

---
+++ scripts/.scripts/kb-layout.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the current layout and variant
+CURRENT_LAYOUT=$(setxkbmap -query | awk '/layout:/ {print $2}')
+VARIANT=$(setxkbmap -query | awk '/variant:/ {print $2}')
+
+# Print the layout and variant if present
+if [[ "$VARIANT"  == "intl" ]]; then
+    CURRENT_LAYOUT="$CURRENT_LAYOUT-$VARIANT"
+fi
+
+echo " $CURRENT_LAYOUT"
