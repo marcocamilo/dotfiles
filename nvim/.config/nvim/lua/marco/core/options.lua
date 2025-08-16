9a9776d184c87cd5210cdc794827190491c32937
Marco Camilo <mac.pietri10@gmail.com>
2025-08-16 23:23:31 +0200

    nvim options optimization

--- nvim/.config/nvim/lua/marco/core/options.lua
+++ nvim/.config/nvim/lua/marco/core/options.lua
@@ -1,57 +1,98 @@
-local g = vim.g -- Global variables
-local opt = vim.opt -- Set options (global/buffer/windows-scoped)
+local opt = vim.opt
 
-opt.backup = false -- creates a backup file
-opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
-opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-opt.conceallevel = 1 -- so that `` is visible in markdown files
-opt.cursorline = true -- highlight the current line
-opt.expandtab = true -- convert tabs to spaces
-opt.fileencoding = "utf-8" -- the encoding written to a file
-opt.fillchars.eob = " " -- show empty lines at the end of a buffer as ` ` {default `~`}
-opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
-opt.helpheight = 15
-opt.hlsearch = true -- highlight all matches on previous search pattern
-opt.ignorecase = true -- ignore case in search patterns
-opt.laststatus = 3 -- only the last window will always have a status line
-opt.linebreak = true -- Wrap on word boundary
-opt.mouse = "a" -- allow the mouse to be used in neovim
-opt.number = true -- set numbered lines
-opt.relativenumber = true -- set relative numbers
-opt.numberwidth = 4 -- minimal number of columns to use for the line number {default 4}
-opt.pumheight = 10 -- pop up menu height
-opt.report = 1 -- print message from commands
-opt.ruler = false -- hide the line and column number of the cursor position
-opt.scrolloff = 4 -- minimal number of screen lines to keep above and below the cursor
-opt.sidescrolloff = 8 -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
-opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
-opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
-opt.showcmd = false -- hide (partial) command in the last line of the screen (for performance)
-opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
-opt.showtabline = 0 -- always show tabs
-opt.smartcase = true -- smart case
-opt.smartindent = true -- make indenting smarter again
-opt.splitbelow = true -- force all horizontal splits to go below current window
-opt.splitright = true -- force all vertical splits to go to the right of current window
-opt.swapfile = false -- creates a swapfile
-opt.tabstop = 2 -- insert 2 spaces for a tab
-opt.termguicolors = true -- set term gui colors (most terminals support this)
-opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
-opt.undofile = true -- enable persistent undo
-opt.updatetime = 300 -- faster completion (4000ms default)
-opt.winheight = 20
-opt.wrap = false -- display lines as one long line
-opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+-- ⚡ REDRAW & DISPLAY PERFORMANCE
+opt.ttyfast = true        -- Fast terminal connection for responsive display
+opt.updatetime = 250      -- CursorHold event trigger delay in ms (your 300)
+opt.timeoutlen = 500      -- Mapped sequence completion timeout in ms (your 1000)
+opt.redrawtime = 5000     -- Max time for syntax highlighting before timeout
 
-opt.shortmess:append("c") -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
-opt.whichwrap:append("<,>,[,],h,l") -- keys allowed to move to the previous/next line when the beginning/end of line is reached
-opt.iskeyword:append("-") -- treats words with `-` as single words
-opt.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
+-- 🧠 MEMORY & PROCESSING LIMITS
+opt.maxmempattern = 5000  -- Max memory in KB for pattern matching
+opt.synmaxcol = 240       -- Max column for syntax highlighting
+opt.history = 1000        -- Command lines to remember in history
 
-opt.backspace = "indent,eol,start"
-opt.foldmethod = "syntax"
+-- 📱 USER INTERFACE LAYOUT
+opt.showtabline = 0       -- When to show tab pages: 0=never, 1=multiple, 2=always
+opt.laststatus = 3        -- When to show status line: 3=global for all windows
+opt.cmdheight = 1         -- Screen lines for command-line area
+opt.showcmd = false       -- Show partial command in last line
+opt.showmode = false      -- Show current mode in command line
+opt.ruler = false         -- Show cursor line/column in status
 
--- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- vim.cmd([[ set nofoldenable ]])
+-- 📊 LINE NUMBERS & POSITIONING
+opt.number = true         -- Show line numbers in left margin
+opt.relativenumber = true -- Show relative line numbers from current
+opt.numberwidth = 4       -- Min columns for line number display
+opt.signcolumn = "yes"    -- Always show sign column
+opt.cursorline = true     -- Highlight current cursor line
+
+-- 🎛️ WINDOW & POPUP BEHAVIOR
+opt.pumheight = 10        -- Max items in popup menu
+opt.winminheight = 1      -- Min height for non-current windows
+opt.equalalways = false   -- Don't auto-resize windows after split/close
+opt.winheight = 20        -- Min height for current window
+opt.helpheight = 15       -- Initial height of help window
+
+-- 📏 SCROLLING & VIEWPORT
+opt.scrolloff = 4         -- Min lines above/below cursor
+opt.sidescrolloff = 8     -- Min columns left/right of cursor
+opt.wrap = false          -- Don't wrap long lines
+opt.linebreak = true      -- Break lines at word boundaries when wrapping
+
+-- 📂 CODE FOLDING CONFIGURATION
+opt.foldmethod = "syntax" -- Fold method: syntax-based folding
+opt.fillchars = {         -- Characters for statuslines and separators
+  eob = " ",              -- Empty lines at end of buffer
+  fold = " ",             -- Closed fold lines
+  foldopen = " ",         -- Open fold indicator
+  foldsep = " ",          -- Fold column separator
+  foldclose = " ",        -- Closed fold indicator
+  diff = "╱",             -- Deleted lines in diff mode
+  msgsep = "‾",           -- Message separator
+}
+
+-- ⌨️ EDITING BEHAVIOR
+opt.expandtab = true      -- Convert tabs to spaces
+opt.tabstop = 2           -- Spaces per tab character
+opt.shiftwidth = 2        -- Spaces per indentation level
+opt.smartindent = true    -- Smart autoindenting on new lines
+opt.conceallevel = 1      -- Conceal level for special syntax
+opt.mouse = "a"           -- Enable mouse in all modes
+opt.backspace = "indent,eol,start" -- Backspace behavior in insert mode
+
+-- 🔍 SEARCH & PATTERN MATCHING
+opt.ignorecase = true     -- Case insensitive search
+opt.smartcase = true      -- Case sensitive if uppercase in pattern
+opt.hlsearch = true       -- Highlight all search matches
+opt.incsearch = true      -- Show partial matches while typing
+
+-- 📁 FILE & ENCODING
+opt.fileencoding = "utf-8" -- File encoding for writing
+opt.clipboard = "unnamedplus" -- Use system clipboard
+opt.report = 1            -- Threshold for reporting changed lines
+
+-- 💾 FILE BACKUP & RECOVERY
+opt.backup = false        -- Don't create backup files
+opt.writebackup = false   -- Don't create backup before overwrite
+opt.swapfile = false      -- Don't use swap files
+opt.undofile = true       -- Save undo history to file
+opt.undodir = vim.fn.stdpath("cache") .. "/undo" -- Undo files directory
+
+-- 📝 COMPLETION BEHAVIOR
+opt.completeopt = { "menuone", "noselect" } -- Insert mode completion options
+
+-- 🎨 VISUAL APPEARANCE & COLORS
+opt.termguicolors = true  -- Enable 24-bit RGB colors
+opt.pumblend = 10         -- Popup menu transparency (0=opaque, 100=transparent)
+opt.winblend = 0          -- Floating window transparency
+opt.guifont = "monospace:h17" -- Font for graphical Neovim
+
+-- 🎯 SPLIT WINDOW BEHAVIOR
+opt.splitbelow = true     -- Horizontal splits go below current
+opt.splitright = true     -- Vertical splits go right of current
+
+-- 📝 TEXT FORMATTING
+opt.formatoptions:remove({ "c", "r", "o" }) -- Remove auto-commenting
+opt.shortmess:append("c")  -- Hide completion match messages
+opt.whichwrap:append("<,>,[,],h,l") -- Keys that wrap to next/prev line
+opt.iskeyword:append("-") -- Treat hyphenated words as single words
