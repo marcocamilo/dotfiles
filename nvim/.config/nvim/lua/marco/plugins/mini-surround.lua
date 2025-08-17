e2561ffb6113a01eb0c0f5bde5c45917a120aed1
Marco Camilo <mac.pietri10@gmail.com>
2025-08-17 23:52:53 +0200

    nvim-opt: surround and substitute modernizations

--- nvim/.config/nvim/lua/marco/plugins/mini-surround.lua
+++ nvim/.config/nvim/lua/marco/plugins/mini-surround.lua
@@ -1,6 +1,14 @@
 return {
-  "echasnovski/mini.surround",
-  -- event = { "BufReadPre", "BufNewFile" },
+	"echasnovski/mini.surround",
+	keys = {
+		{ "sa", desc = "Add surrounding" },
+		{ "sd", desc = "Delete surrounding" },
+		{ "sf", desc = "Find surrounding" },
+		{ "sF", desc = "Find surrounding (backward)" },
+		{ "sh", desc = "Highlight surrounding" },
+		{ "sr", desc = "Replace surrounding" },
+		{ "sn", desc = "Update `n_lines`" },
+	},
   version = "*",
   config = true
 }
