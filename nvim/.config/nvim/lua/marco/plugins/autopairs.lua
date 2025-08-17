e618ca857fd372056d87922c73b2e411b43d26f6
Marco Camilo <mac.pietri10@gmail.com>
2025-08-17 15:00:00 +0200

    nvim-opt: autopairs optimization, cmp removal, lazy load comment + eyelinere

--- nvim/.config/nvim/lua/marco/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/marco/plugins/autopairs.lua
@@ -1,20 +1,23 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	dependencies = "hrsh7th/nvim-cmp",
 	config = function()
-		-- Setup autopairs with essential configurations
-		require("nvim-autopairs").setup({
-			check_ts = true,
-			disable_filetype = { "TelescopePrompt" },
+		local npairs = require("nvim-autopairs")
+
+		npairs.setup({
+			check_ts = true, -- Use treesitter for better pair detection
 			ts_config = {
 				lua = { "string", "source" },
 				javascript = { "string", "template_string" },
+				python = { "string", "f_string" },
 			},
+			disable_filetype = { "TelescopePrompt", "spectre_panel" },
 			fast_wrap = {
 				map = "<M-e>",
 				chars = { "{", "[", "(", '"', "'" },
 				pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+				offset = 0,
+				end_key = "$",
 				keys = "qwertyuiopzxcvbnmasdfghjkl",
 				check_comma = true,
 				highlight = "PmenuSel",
@@ -22,62 +25,21 @@ return {
 			},
 		})
 
-		-- Rules
+		-- Custom rules for specific filetypes
 		local Rule = require("nvim-autopairs.rule")
-		local npairs = require("nvim-autopairs")
+		local cond = require("nvim-autopairs.conds")
 
+		-- Markdown rules
 		npairs.add_rules({
-			Rule("*", "*", "markdown")
-				:with_pair(function(opts)
-					-- Only pair when not followed by another *
-					local next_char = opts.line:sub(opts.col + 1, opts.col + 1)
-					return next_char ~= "*"
-				end)
-				:with_move(function(opts)
-					return opts.prev_char:match(".%*") ~= nil
-				end)
-				:use_key("*"),
+			Rule("*", "*", "markdown"):with_pair(cond.not_after_regex("*", 1)):with_move(function(opts)
+				return opts.prev_char:match(".%*") ~= nil
+			end),
 		})
 
-		-- Integrate with nvim-cmp for autocompletion
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		require("cmp").event:on(
-			"confirm_done",
-			cmp_autopairs.on_confirm_done({
-				filetypes = {
-					["*"] = {
-						["("] = {
-							kind = {
-								require("cmp").lsp.CompletionItemKind.Function,
-								require("cmp").lsp.CompletionItemKind.Method,
-							},
-						},
-					},
-					lua = {
-						["("] = {
-							kind = {
-								require("cmp").lsp.CompletionItemKind.Function,
-								require("cmp").lsp.CompletionItemKind.Method,
-							},
-							handler = function(char, item, bufnr, rules, commit_character) end,
-						},
-					},
-					rmd = {
-						["{"] = {
-							kind = {
-								require("cmp").lsp.CompletionItemKind.Function,
-								require("cmp").lsp.CompletionItemKind.Method,
-							},
-						},
-					},
-					tex = false, -- Disable autopairs for tex
-			},
-		}))
-
-		-- LaTeX-specific autopairs rules
-		require("nvim-autopairs").add_rules({
-			require("nvim-autopairs.rule")("$", "$", "tex"),
-			require("nvim-autopairs.rule")("\\[", "\\]", "tex"),
+		-- LaTeX rules
+		npairs.add_rules({
+			Rule("$", "$", "tex"):with_pair(cond.not_filetypes({ "vim" })),
+			Rule("\\[", "\\]", "tex"),
 		})
 	end,
 }
