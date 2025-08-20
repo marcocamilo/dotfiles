f9bb8d9831d1a8cbba02dcaf7dc294ee58d4c7f9
Marco Camilo <mac.pietri10@gmail.com>
2025-08-20 23:05:36 +0200

    nvim-opt: blink disable in DressingInput

--- nvim/.config/nvim/lua/marco/plugins/blink.lua
+++ nvim/.config/nvim/lua/marco/plugins/blink.lua
@@ -1,42 +1,37 @@
 return {
 	"saghen/blink.cmp",
+	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = {
 		"saghen/blink.compat",
 		"rafamadriz/friendly-snippets",
 		"L3MON4D3/LuaSnip",
 		{
 			"moyiz/blink-emoji.nvim",
-
-			config = function()
-				-- Get the original module
+			opts = {
+				trigger = ";",
+			},
+			config = function(_, opts)
 				local emoji_source = require("blink-emoji")
-
-				-- Override the get_trigger_characters function
-				emoji_source.get_trigger_characters = function(self)
-					return { ";" }
+				emoji_source.get_trigger_characters = function()
+					return { opts.trigger or ";" }
 				end
 			end,
 		},
 	},
-
 	version = "1.*",
-	---
+
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		-- keymap = { preset = 'super-tab' },
+
+    -- Disable in DressingInput
+		enabled = function()
+			return not vim.tbl_contains({}, vim.bo.filetype)
+				and vim.bo.buftype ~= "nofile"
+				and vim.bo.buftype ~= "prompt"
+				and vim.b.completion ~= false
+		end,
+
 		keymap = {
 			["<CR>"] = { "accept", "fallback" },
 			["<Tab>"] = { "hide", "fallback" },
