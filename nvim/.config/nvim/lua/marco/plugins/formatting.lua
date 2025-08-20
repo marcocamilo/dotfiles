41afdb299a5df2c9629b8d29f98652d6e5c28ddf
Marco Camilo <mac.pietri10@gmail.com>
2025-08-21 00:16:39 +0200

    nvim-opt: lsp modernization to neovim 0.11 + formatting opt
    - Removed lsp-zero
    - nvim native setup
    - formatting optimized

--- nvim/.config/nvim/lua/marco/plugins/formatting.lua
+++ nvim/.config/nvim/lua/marco/plugins/formatting.lua
@@ -1,30 +1,29 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			formatters_by_ft = {
-        cpp = { "clang-format" },
-				javascript = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "biome" },
-				jsonc = { "biome" },
-				lua = { "stylua" },
-				markdown = { "mdformat" },
-				python = { "ruff_fix", "ruff_format", "isort" },
-				yaml = { "prettier" },
-			},
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 5000,
-			})
-		end, { desc = "Make pretty with formatter" })
-	end,
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      cpp = { "clang-format" },
+      javascript = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "biome" },
+      jsonc = { "biome" },
+      lua = { "stylua" },
+      markdown = { "mdformat" },
+      python = { "ruff_fix", "ruff_format", "isort" },
+      yaml = { "prettier" },
+    },
+  },
+  keys = {
+    {
+      "<leader>mp",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = { "n", "v" },
+      desc = "Make pretty with formatter",
+    },
+  },
 }
