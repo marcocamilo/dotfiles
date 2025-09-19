593e1475b32f901de15218d9a171e5553df1fab8
Marco Camilo <mac.pietri10@gmail.com>
2025-09-20 00:24:51 +0200

    Squash merge dmenu-reset into main

--- dwm/.config/suckless/dmenu/config.def.h
+++ dwm/.config/suckless/dmenu/config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"MesloLGS Nerd Font:size=10"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,8 +14,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 20;
-static unsigned int columns    = 1;
+static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
