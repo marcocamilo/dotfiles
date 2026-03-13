fb11d1aa8ae7b21afc3c6eb89d94aa4ee5bdf405
Marco Camilo <mac.pietri10@gmail.com>
2026-03-13 17:11:11 +0100

    markdown renderer + color definitions

---
+++ nvim/.config/nvim/lua/marco/colors.lua
@@ -0,0 +1,61 @@
+local colors = {
+	-- Background shades
+	bg = "#000000",
+	bg_dark = "#0a0a12",
+	bg_deep = "#141b22",
+	bg_highlight = "#1a1b2e",
+	bg_visual = "#e9b3fd",
+	bg_statusline = "#0a0a12",
+
+	-- Foreground shades
+	fg = "#ffffff",
+	fg_dark = "#ffffff",
+	fg_gutter = "#232e3b",
+	comment = "#b7bfce",
+
+	-- Core palette
+	red = "#f16c75",
+	orange = "#e58f2a",
+	yellow = "#9ad900",
+	yellow_light = "#f1fc79",
+	green = "#37f499",
+	cyan = "#04d1f9",
+	blue = "#5b9cf5",
+	purple = "#987afb",
+	purple_light = "#c76bfa",
+	magenta = "#fca6ff",
+	magenta_light = "#f94dff",
+	pink = "#fca6ff",
+	burgundy = "#e05577",
+
+	red_dark = "#3a1218",
+	orange_dark = "#2d1c08",
+	yellow_dark = "#1e2b00",
+	green_dark = "#10492d",
+	cyan_dark = "#0a3d3d",
+	blue_dark = "#0d2a4a",
+	purple_dark = "#2d244b",
+	magenta_dark = "#4b314c",
+	burgundy_dark = "#3d1225",
+
+	-- UI elements
+	ui_border = "#04d1f9",
+	ui_menu = "#000000",
+	ui_nontext = "#232e3b",
+	ui_terminal_black = "#141b22",
+
+	-- Git
+	git_change = "#04d1f9",
+	git_add = "#37f499",
+	git_delete = "#f16c75",
+
+	-- Markdown heading backgrounds
+	head1_bg = "#10492d",
+	head2_bg = "#0a3d3d",
+	head3_bg = "#0d2a4a",
+	head4_bg = "#2d244b",
+	head5_bg = "#3a1f4b",
+	head6_bg = "#3d1225",
+}
+
+return colors
--- nvim/.config/nvim/lua/marco/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/marco/plugins/colorschemes.lua
@@ -1,129 +1,164 @@
+local colors = require("marco.colors")
+
 return {
-  {
-    "eldritch-theme/eldritch.nvim",
-    name = "eldritch",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = false,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      on_colors = function(global_colors)
-        local color_definitions = {
-          -- Background
-          bg = "#000000",
-          bg_dark = "#0a0a12",
-          bg_highlight = "#1a1b2e",
-          bg_visual = "#e9b3fd",
-          bg_statusline = "#0a0a12",
+	{
+		"eldritch-theme/eldritch.nvim",
+		name = "eldritch",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = false,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+			on_colors = function(global_colors)
+				local color_definitions = {
+					-- Background
+					bg = colors.bg,
+					bg_dark = colors.bg_dark,
+					bg_highlight = colors.bg_highlight,
+					bg_visual = colors.bg_visual,
+					bg_statusline = colors.bg_statusline,
 
-          -- Foreground
-          fg = "#ffffff",
-          fg_dark = "#ffffff",
-          fg_gutter = "#232e3b",
-          comment = "#b7bfce",
+					-- Foreground
+					fg = colors.fg,
+					fg_dark = colors.fg_dark,
+					fg_gutter = colors.fg_gutter,
+					comment = colors.comment,
 
-          -- Core palette
-          green = "#37f499",
-          cyan = "#04d1f9",
-          purple = "#987afb",
-          pink = "#fca6ff",
-          yellow = "#9ad900",
-          red = "#f16c75",
-          orange = "#e58f2a",
+					-- Core palette
+					red = colors.red,
+					orange = colors.orange,
+					yellow = colors.yellow,
+					green = colors.green,
+					cyan = colors.cyan,
+					blue = colors.blue,
+					purple = colors.purple,
+					magenta = colors.magenta,
+					pink = colors.pink,
 
-          -- Bright variants
-          bright_red = "#f16c75",
-          bright_green = "#37f499",
-          bright_yellow = "#9ad900",
-          bright_blue = "#987afb",
-          bright_magenta = "#fca6ff",
-          bright_cyan = "#04d1f9",
-          bright_white = "#ffffff",
+					-- Bright variants
+					bright_red = colors.red,
+					bright_green = colors.green,
+					bright_yellow = colors.yellow,
+					bright_blue = colors.purple,
+					bright_magenta = colors.magenta,
+					bright_cyan = colors.cyan,
+					bright_white = colors.fg,
 
-          -- UI elements
-          border = "#04d1f9",
-          menu = "#000000",
-          visual = "#e9b3fd",
-          selection = "#e9b3fd",
-          gutter_fg = "#232e3b",
-          nontext = "#232e3b",
-          white = "#ffffff",
-          black = "#000000",
-          terminal_black = "#141b22",
-          dark3 = "#141b22",
-          dark5 = "#141b22",
-          dark_cyan = "#04d1f9",
-          dark_green = "#37f499",
-          dark_yellow = "#9ad900",
-          magenta = "#fca6ff",
-          magenta2 = "#fca6ff",
-          magenta3 = "#fca6ff",
+					-- UI elements
+					border = colors.ui_border,
+					menu = colors.ui_menu,
+					visual = colors.bg_visual,
+					selection = colors.bg_visual,
+					gutter_fg = colors.fg_gutter,
+					nontext = colors.ui_nontext,
+					white = colors.fg,
+					black = colors.bg,
+					terminal_black = colors.bg_deep,
+					dark3 = colors.bg_deep,
+					dark5 = colors.bg_deep,
+					dark_cyan = colors.cyan,
+					dark_green = colors.green,
+					dark_yellow = colors.yellow,
 
-          -- Git
-          git = {
-            change = "#04d1f9",
-            add = "#37f499",
-            delete = "#f16c75",
-          },
-          gitSigns = {
-            change = "#04d1f9",
-            add = "#37f499",
-            delete = "#f16c75",
-          },
-        }
+					-- Git
+					git = {
+						change = colors.git_change,
+						add = colors.git_add,
+						delete = colors.git_delete,
+					},
+					gitSigns = {
+						change = colors.git_change,
+						add = colors.git_add,
+						delete = colors.git_delete,
+					},
+				}
 
-        for key, value in pairs(color_definitions) do
-          global_colors[key] = value
-        end
-      end,
+				for key, value in pairs(color_definitions) do
+					global_colors[key] = value
+				end
+			end,
 
-      on_highlights = function(hl)
-        local highlight_definitions = {
-          -- Cursor line
-          CursorLine = { bg = "#141b22" },
+			on_highlights = function(hl)
+				local highlight_definitions = {
+					-- Cursor line
+					CursorLine = { bg = colors.bg_deep },
 
-          -- Visual selection
-          Visual = { bg = "#e9b3fd", fg = "#000000" },
+					-- Visual selection
+					Visual = { bg = colors.bg_visual, fg = colors.bg },
 
-          -- Markdown
-          ["@markup.strong"] = { fg = "#f94dff", bold = true },
-          ["@markup.raw.markdown_inline"] = { fg = "#fca6ff" },
+					-- Markdown
+					["@markup.strong"] = { fg = colors.green, bold = true },
+					["@markup.raw.markdown_inline"] = { fg = colors.magenta },
+					["@markup.heading.1.markdown"] = { fg = colors.green, bg = colors.head1_bg, bold = true },
+					["@markup.heading.2.markdown"] = { fg = colors.cyan, bg = colors.head2_bg, bold = true },
+					["@markup.heading.3.markdown"] = { fg = colors.blue, bg = colors.head3_bg, bold = true },
+					["@markup.heading.4.markdown"] = { fg = colors.purple, bg = colors.head4_bg, bold = true },
+					["@markup.heading.5.markdown"] = { fg = colors.purple_light, bg = colors.head5_bg, bold = true },
+					["@markup.heading.6.markdown"] = { fg = colors.burgundy, bg = colors.head6_bg, bold = true },
+          ["@markup.strikethrough"] = { strikethrough = true, fg = colors.comment },
 
-          -- Spell
-          SpellBad = { sp = "#f16c75", undercurl = true, bold = true, italic = true },
-          SpellCap = { sp = "#f1fc79", undercurl = true, bold = true, italic = true },
+					-- Spell
+					SpellBad = { sp = colors.red, undercurl = true, bold = true, italic = true },
+					SpellCap = { sp = colors.yellow_light, undercurl = true, bold = true, italic = true },
 
-          -- Colorcolumn
-          ColorColumn = { bg = "#141b22" },
+					-- Colorcolumn
+					ColorColumn = { bg = colors.bg_deep },
 
-          -- Folded lines
-          Folded = { bg = "NONE" },
+					-- Folded lines
+					Folded = { bg = "NONE" },
 
-          -- Floats and borders
-          FloatBorder = { fg = "#37f499", bg = "#000000" },
-          NormalFloat = { bg = "#0a0a12" },
+					-- Floats and borders
+					FloatBorder = { fg = colors.green, bg = colors.bg },
+					NormalFloat = { bg = colors.bg_dark },
 
-          -- Noice cmdline
-          NoiceCmdlineIconCmdline = { fg = "#04d1f9" },
-          NoiceCmdlinePopupBorderCmdline = { fg = "#37f499" },
+					-- Noice cmdline
+					NoiceCmdlineIconCmdline = { fg = colors.cyan },
+					NoiceCmdlinePopupBorderCmdline = { fg = colors.green },
 
-          -- Winbar (for later use)
-          WinBar1 = { fg = "#04d1f9" },
-          WinBar2 = { fg = "#37f499" },
-          WinBar3 = { fg = "#f94dff", bold = true },
-        }
+					-- Winbar (for later use)
+					WinBar1 = { fg = colors.cyan },
+					WinBar2 = { fg = colors.green },
+					WinBar3 = { fg = colors.magenta_light, bold = true },
 
-        for group, props in pairs(highlight_definitions) do
-          hl[group] = props
-        end
-      end,
-    },
-    config = function(_, opts)
-      require("eldritch").setup(opts)
-      vim.cmd.colorscheme("eldritch")
-    end,
-  },
+					-- Markdown heading backgrounds (for render-markdown.nvim)
+					Headline1Bg = { fg = colors.green, bg = colors.head1_bg },
+					Headline2Bg = { fg = colors.cyan, bg = colors.head2_bg },
+					Headline3Bg = { fg = colors.blue, bg = colors.head3_bg },
+					Headline4Bg = { fg = colors.purple, bg = colors.head4_bg },
+					Headline5Bg = { fg = colors.purple_light, bg = colors.head5_bg },
+					Headline6Bg = { fg = colors.burgundy, bg = colors.head6_bg },
+
+					Headline1Fg = { fg = colors.green, bold = true },
+					Headline2Fg = { fg = colors.cyan, bold = true },
+					Headline3Fg = { fg = colors.blue, bold = true },
+					Headline4Fg = { fg = colors.purple, bold = true },
+					Headline5Fg = { fg = colors.purple_light, bold = true },
+					Headline6Fg = { fg = colors.burgundy, bold = true },
+
+					-- Codeblocks for render-markdown
+					RenderMarkdownCode = { bg = colors.bg_deep },
+					RenderMarkdownCodeInline = { fg = colors.green, bg = colors.bg_deep },
+
+          -- Callout / blockquote colors for render-markdown
+          RenderMarkdownQuote = { fg = colors.blue, bg = colors.blue_dark, bold = true },
+          RenderMarkdownInfo = { fg = colors.blue, bg = colors.blue_dark, bold = true },
+          RenderMarkdownWarn = { fg = colors.orange, bg = colors.orange_dark, bold = true },
+          RenderMarkdownError = { fg = colors.red, bg = colors.red_dark, bold = true },
+          RenderMarkdownHint = { fg = colors.orange, bg = colors.orange_dark, bold = true },
+          RenderMarkdownSuccess = { fg = colors.green, bg = colors.green_dark, bold = true },
+				}
+
+				for group, props in pairs(highlight_definitions) do
+					hl[group] = props
+				end
+			end,
+		},
+		config = function(_, opts)
+			require("eldritch").setup(opts)
+			vim.cmd.colorscheme("eldritch")
+		end,
+	},
 }
